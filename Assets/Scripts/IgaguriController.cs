d24184192d142dff3fac448f80d5494e6b7706b2
Billy Huang <10646003@ntub.edu.tw>
2020-03-08 00:53:52 +0800

    7-6 - 建立製造栗子的工廠

--- Assets/Scripts/IgaguriController.cs
+++ Assets/Scripts/IgaguriController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class IgaguriController : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
-        Shoot(new Vector3(0, 200, 2000));
+        // Shoot(new Vector3(0, 200, 2000));
     }
 
     // Update is called once per frame
